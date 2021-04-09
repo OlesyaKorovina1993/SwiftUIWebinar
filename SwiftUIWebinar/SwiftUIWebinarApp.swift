4bce7a1cd0fdfb95002afa0abd3f0b1f1241f17d
Olesia Maslennikova <olesya_korovina_1993@mail.ru>
2021-04-09 14:19:52 +0300

    SwiftUIWebinar

---
+++ SwiftUIWebinar/SwiftUIWebinarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWebinarApp.swift
+//  SwiftUIWebinar
+//
+//  Created by user on 09.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWebinarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
